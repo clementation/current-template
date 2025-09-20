0d7dd868653c741beee9e86e02312a0751e8c731
Connor Kealey <creations@connorkealey.design>
2025-09-19 21:05:01 -0700

    v1 of the secret rabbit page

--- src/routes/super-secret-rabbit-page.tsx
+++ src/routes/super-secret-rabbit-page.tsx
@@ -12,7 +12,10 @@ function RouteComponent() {
   return(
 
     <div className="auto-wrapper">
-      <div className="auto-wrapper flex-column no-padding">
+      <div className="auto-wrapper flex-column no-padding stay">
+        <div className="text-wrapper secondary me">
+          <h1>welcome</h1>
+        </div>
         <div className="image-wrapper me">
           <LazyImage src="/actually-me.jpg" alt="a cream colored bunny rabbit with circular glasses drawn on"/>
         </div>
@@ -20,10 +23,19 @@ function RouteComponent() {
           <h1>(actually me)</h1>
         </div>
       </div>
-      <div className="auto-wrapper">
+      <div className="auto-wrapper flex-column gap">
         <div className="text-wrapper secondary">
-          <h2> bunnies of cannon beach</h2>
+          <h1>bunnies of cannon beach</h1>
         </div>
+          <LazyImage src="./bunny-photos/bunny photos-1.jpg" alt="a really cute bunny" />
+          <LazyImage src="./bunny-photos/bunny photos-2.jpg" alt="a really cute bunny" />
+          <LazyImage src="./bunny-photos/bunny photos-3.jpg" alt="a really cute bunny" />
+          <LazyImage src="./bunny-photos/bunny photos-4.jpg" alt="a really cute bunny" />
+          <LazyImage src="./bunny-photos/bunny photos-5.jpg" alt="a really cute bunny" />
+          <LazyImage src="./bunny-photos/bunny photos-6.jpg" alt="a really cute bunny" />
+          <LazyImage src="./bunny-photos/bunny photos-7.jpg" alt="a really cute bunny" />
+          <LazyImage src="./bunny-photos/bunny photos-8.jpg" alt="a really cute bunny" />
+          <LazyImage src="./bunny-photos/bunny photos-9.jpg" alt="a really cute bunny" />
       </div>
     </div>
   )
